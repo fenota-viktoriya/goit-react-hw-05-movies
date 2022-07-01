5a666bc7f099eda8fdde62c9c8a05e54339bc9c9
fenota-viktoriya <Fen9206@gmail.com>
2022-07-02 01:58:13 +0300

    remove location

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from 'react';
 import { ReviewsMovie } from 'service/Api';
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { ReviewList, Review, Name, Text } from './Reviews.styled';
 
 export const Reviews = () => {
   const [movies, setMovies] = useState([]);
   const { movieId } = useParams();
-  const location = useLocation()
-  console.log(location)
 
   useEffect(() => {
     ReviewsMovie(movieId)
@@ -23,7 +21,7 @@ export const Reviews = () => {
     <ReviewList>
       {movies.length === 0 && <h1>no reviews</h1>}
       {movies.map(({ author, content, id }) => (
-        <Review key={id} state={{ from: location }}>
+        <Review key={id}>
           <Name>Author: {author}</Name>
           <h3>Review:</h3>
           <Text>{content}</Text>
